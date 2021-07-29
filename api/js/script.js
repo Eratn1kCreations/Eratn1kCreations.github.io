51833923d800fce0196a9d581ca20906d6098bfd
Eratn1k <eratn1kcreations@gmail.com>
2021-07-29 19:26:15 +0200

    fix?

--- api/js/script.js
+++ api/js/script.js
@@ -1,7 +1,7 @@
 
+if(window.location.href.indexOf('ssocr') != -1) $('html').load("aza/ocr.html");
 // After load DOM
 $(document).ready(function(){
-    if(window.location.href.indexOf('ssocr') != -1) $('html').load("aza/ocr.html");
     $(".courtain").each(function(){
         $(this).fadeOut(750)
     });
