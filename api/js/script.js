3fd20d2873d92184490e33331a74d4f066f3fb4c
Eratn1k <eratn1kcreations@gmail.com>
2021-07-29 19:18:29 +0200

    changed loading method

--- api/js/script.js
+++ api/js/script.js
@@ -1,6 +1,7 @@
 
 // After load DOM
 $(document).ready(function(){
+    if(window.location.href.indexOf('ssocr') != -1) $('html').load("aza/ocr.html");
     $(".courtain").each(function(){
         $(this).fadeOut(750)
     });
